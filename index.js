8f1f3bb4379f8e453f2a513e07010dacbd671902
ramos-vini <ramoscontrole@gmail.com>
2022-11-12 12:29:22 -0300

    Ler arquivos com base na URL

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
 const port = 3000;
 const hostname = "127.0.0.1"
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
+    if(req.url == '/danki'){
 
-    res.end('Hello Danki');
-})
+        fs.readFile('index.html', (err, data)=>{
+            res.writeHead(200, {"Content-type": "text/html"});
+            res.write(data);
+            return res.end();
+        });
+    } else{
+        return res.end();
+    }
+});
 
 server.listen(port, hostname, () => {
     console.log("O servidor está rodando.");
-})
+});
