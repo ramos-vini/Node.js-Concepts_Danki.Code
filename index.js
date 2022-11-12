b3e2a25a33d5815b0581d3058218f5d05915cf2d
ramos-vini <ramoscontrole@gmail.com>
2022-11-12 12:59:15 -0300

    Leitura simples de arquivos

--- index.js
+++ index.js
@@ -1,35 +1,5 @@
-const http = require('http');
 const fs = require('fs');
 
-const port = 3000;
-const hostname = "127.0.0.1"
-
-// Criando novo arquivo
-// fs.writeFile('danki.txt', 'Conteúdo Primário', (err) => {
-//     if (err) throw err;
-//     console.log('Arquivo .txt criado com sucesso!');
-// });
-
-const server = http.createServer((req, res) => {
-    if (req.url == '/danki') {
-
-        fs.readFile('index.html', (err, data) => {
-
-            // Criando novo arquivo ou adicionando conteúdo (caso arquivo já exista)
-            fs.appendFile('danki.txt', '\nConteúdo Adicionado', (err) => {
-                if (err) throw err;
-                console.log('Conteúdo adicionado com sucesso!');
-            });
-
-            res.writeHead(200, { "Content-type": "text/html" });
-            res.write(data);
-            return res.end();
-        });
-    } else {
-        return res.end();
-    }
-});
-
-server.listen(port, hostname, () => {
-    console.log("O servidor está rodando.");
-});
+fs.readFile('danki.txt', (err, data) => {
+    console.log(data.toString());
+})
