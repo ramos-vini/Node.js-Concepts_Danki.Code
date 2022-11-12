a65d6c90343bf8447bddb1631578e6a9a4f41d63
ramos-vini <ramoscontrole@gmail.com>
2022-11-12 12:47:55 -0300

    Criando Arquivos

--- index.js
+++ index.js
@@ -4,15 +4,28 @@ const fs = require('fs');
 const port = 3000;
 const hostname = "127.0.0.1"
 
-const server = http.createServer((req, res) => {
-    if(req.url == '/danki'){
+// Criando novo arquivo
+// fs.writeFile('danki.txt', 'Conteúdo Primário', (err) => {
+//     if (err) throw err;
+//     console.log('Arquivo .txt criado com sucesso!');
+// });
 
-        fs.readFile('index.html', (err, data)=>{
-            res.writeHead(200, {"Content-type": "text/html"});
+const server = http.createServer((req, res) => {
+    if (req.url == '/danki') {
+
+        fs.readFile('index.html', (err, data) => {
+
+            // Criando novo arquivo ou adicionando conteúdo (caso arquivo já exista)
+            fs.appendFile('danki.txt', '\nConteúdo Adicionado', (err) => {
+                if (err) throw err;
+                console.log('Conteúdo adicionado com sucesso!');
+            });
+
+            res.writeHead(200, { "Content-type": "text/html" });
             res.write(data);
             return res.end();
         });
-    } else{
+    } else {
         return res.end();
     }
 });
