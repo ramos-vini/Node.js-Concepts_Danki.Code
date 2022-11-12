51a2cda5f33acdd763acfaf1d18917e67dea12a3
ramos-vini <ramoscontrole@gmail.com>
2022-11-12 11:59:26 -0300

    Criando servidor node, módulo http e mais

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello Node.js!");
+const http = require('http');
+
+const port = 3000;
+const hostname = "127.0.0.1"
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/plain');
+
+    res.end('Hello Danki');
+})
+
+server.listen(port, hostname, () => {
+    console.log("O servidor está rodando.");
+})
